36b011d64ea8185146f79e8a315bd0e6190d9ed5
hamuham <uni.uni.yui@hotmail.co.jp>
2022-05-05 14:55:39 +0900

    大枠のイメージとgitテスト

---
+++ resources/views/play/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body style="width: 30rem; margin:auto;">
+    <header>
+        <a href="/">トップに戻る</a>
+    </header>
+    <h3>クイズを作る</h3>
+    <div>
+        <div class="card-body">
+            <h2>選択問題</h2>
+            <p class="card-text">自分だけのクイズを作ってみよう！</p>
+            <div>
+                <h2>クイズのタイトルを入力してください</h2>
+                <input name="quiz_title" type="text" style="width:20rem;" maxlength="40">
+                <h3>クイズの選択肢を入力してください</h3>
+                <ul class="list-group">
+                    1:<input name="quiz_title" type="text" class="list-group-item list-group-item-action" maxlength="20"><br>
+                    2:<input name="quiz_title" type="text" class="list-group-item list-group-item-action" maxlength="20"><br>
+                    3:<input name="quiz_title" type="text" class="list-group-item list-group-item-action" maxlength="20"><br>
+                    4:<input name="quiz_title" type="text" class="list-group-item list-group-item-action" maxlength="20"><br>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
